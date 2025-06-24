cbb420fa68ef0f718192a08d7964ce0e39afb934
Jeremy Simmons <101369930+novaTehnika@users.noreply.github.com>
2025-06-24 14:24:15 -0500

    Added initial analysis of linear spring

--- springDesign.m
+++ springDesign.m
@@ -35,22 +35,114 @@
 clear
 
 % define parameters of the diffusion process
-
+P_f = 100*101.3e3; % [Pa] target pressure for dissolution
+V_e = 0.5e-3; % [m^3] final mixture volume
+v_prime = 0.05; % change in volume as a fraction of final mixture volume
+lambda = 0; % margin between preload and target pressure
 
 % Vary the diameter of the piston
-d = linspace(1,8,10)*0.0254; % [in -> m]
+d = linspace(1,8,10000)*0.0254; % [in -> m]
 A = pi/4*d.^2;
 
-% Solve for the change in force and failure
+deltay = v_prime*V_e./A; % displacement over dissolution phase
+F_f = P_f*A;
+F_p = (1-lambda)*P_f*A; % preload force
+
+figure
+plot(d/0.0254,deltay/0.0254)
+xlabel('piston diameter (in)')
+ylabel('displacement (in)')
+title('Piston displacement over dissolution phase vs. piston diameter')
+
+figure
+plot(d/0.0254,F_f/4.44822162)
+xlabel('piston diameter (in)')
+ylabel('force (lbf)')
+title('Spring force at target pressure vs. piston diameter')
+
+%% assuming a change in force for a linear spring
+% Vary the diameter of the piston
+d = [2,3,4,5]*0.0254; % [in -> m]
+A = pi/4*d.^2;
+
+deltay = v_prime*V_e./A; % displacement over dissolution phase
+F_f = P_f*A;
+F_p = (1-lambda)*P_f*A; % preload force
+
+%
+maxDifflection = 0.25;
+f_prime = logspace(log10(0.001),log10(0.5),100); % target fractional change in force during dissolution
+F_max = zeros(numel(d),numel(f_prime));
+k_linSpring = zeros(numel(d),numel(f_prime));
+y_p = zeros(numel(d),numel(f_prime));
+L = zeros(numel(d),numel(f_prime));
+
+for i = 1:numel(d)
+    F_max(i,:) = (1+f_prime)*F_f(i);
+    k_linSpring(i,:) = f_prime*F_f(i)/deltay(i); % linear spring constant
+    y_p(i,:) = F_p(i)./k_linSpring(i,:); % preload displacement of linear spring
+    L(i,:) = (deltay(i)+y_p(i,:))/maxDifflection;
+end
+
+figure
+clear leg
+for i = 1:numel(d)
+    loglog(f_prime,y_p(i,:)/0.0254)
+    hold on
+    leg(i) = {[num2str(d(i)/0.0254),'in piston']};
+end
+plot(f_prime([1,end]),deltay(i)*[1,1])
+leg(numel(d)+1) = {'disp. during diss.'};
+grid on
+xlabel('change in force, fraction')
+ylabel('preload displacement (in)')
+title('Preload displacement vs. fraction change in force during dissolution')
+legend(leg)
+
+figure
+clear leg
+for i = 1:numel(d)
+    loglog(f_prime,L(i,:)/0.0254)
+    hold on
+    leg(i) = {[num2str(d(i)/0.0254),'in piston']};
+end
+grid on
+xlabel('change in force, fraction')
+ylabel('unloaded length (in)')
+title('Unloaded spring length vs. fraction change in force during dissolution')
+legend(leg)
+
+figure
+clear leg
+for i = 1:numel(d)
+    loglog(f_prime,k_linSpring(i,:)/4.44822162/0.0254)
+    hold on
+    leg(i) = {[num2str(d(i)/0.0254),'in piston']};
+end
+grid on
+xlabel('change in force, fraction')
+ylabel('spring stiffness (lbf/in)')
+title('Spring stiffness vs. fraction change in force during dissolution')
+legend(leg)
+
+%% Solve for the change in force and failure
 
 % define material
-parMatl.E = 1e3; % [Pa]
+parMatl.E = 200e9; % [Pa]
 parMatl.mu = 0.3;
-parMatl.par.yeildStress; % [Pa]
+parMatl.par.yeildStress = 250e6; % [Pa]
 
 % Determine dominate spring designs
+
+ % Belleville spring
 fun = @(x) bellevilleOpFun(x,1);
 nonlcon = @(x) bellevilleOpFun(x,2);
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+lb = [];
+ub = [];
 x = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon);
 
 % plot change in force over the extent of travel
@@ -74,7 +166,7 @@ function varargout = bellevilleOpFun(x,deltay,parMatl,mode)
 
             % obj 2: installed length at preload
 
-            varargout{1} = 
+            varargout{1} = [];
         case 2 % nonlinear constraints
             % constraint 1: yeild stress not exceeded
              % stress at maximum difflection
